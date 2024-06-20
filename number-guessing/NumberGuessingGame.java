8905f86c6275f427f050f85637745e59e5166c29
sijalabhatttarai <sijalabhattarai012@gmail.com>
2024-06-20 21:17:26 +0545

    lil changes

--- number-guessing/NumberGuessingGame.java
+++ number-guessing/NumberGuessingGame.java
@@ -1,33 +1,68 @@
 import java.util.Scanner;
 import java.util.Random;
 
-public class NumberGuessingGame{
+public class NumberGuessingGame {
 
-	public static void main(String[] args){
-		generateRandom();
-	}//end main method
+    public static void main(String[] args){
+        generateRandom();
+    }//end main method
 
+    public static void generateRandom(){
+        //creating object
+        Random random = new Random();
 
+        //generating a random number between 0-10
+        int randomNumber = random.nextInt(11);
 
-public static void generateRandom(){
-	//creating object
-	int randomNum=new Random();
+        //calling guess method
+        guess(randomNumber);
 
-	//generating a random number between 0-10
-	int randomNum=rand.nextInt(11);
+    }//end generateRandom method
 
-	//calling guess method
-	guess(randomNum);
+    public static void guess(int randomNumber){
+        //creating an object of scanner class
+        Scanner in = new Scanner(System.in);
 
-}//end generateRandom method
+        //welcome message
+        System.out.println("");
+        System.out.println("Number Guessing Game");
+        System.out.println("Guess a number between 0-10");
+        int guess = in.nextInt();
+        System.out.println("");
 
-public static void guess(int randomNum){
-    //creating an object of scanner class
-    Scanner in = new Scanner(System.in);
+        //input validation
+        while (guess < 0 || guess > 10) {
+            System.out.print("Guess a number between 0-10: ");
+            guess = in.nextInt();
+            System.out.println("");
+        }
 
-    //welcome message
-    System.out.println("");
-    System.out.println("Number Guessing Game");
-    System.out.println("Guess a number between 0-10")
-}//end guess method
-}
+        //try again until you guess the correct number
+        int tries = 0;
+        while (guess != randomNumber){
+            tries++;
+            System.out.println("Wrong guess!");
+            System.out.println("Guess Again:");
+            guess = in.nextInt();
+            System.out.println();
+        }
+
+        //Game won
+        System.out.println("Correct guess!");
+        System.out.println("Number of wrong tries: " + tries);
+        System.out.println("");
+
+        //play again or terminate
+        System.out.println("Press 1 to play again.");
+        System.out.println("Press 0 to exit.");
+        //random method will be called again
+        // get user's choice
+        int choice = in.nextInt();
+
+        // random method will be called again
+        if (choice == 1)
+            generateRandom();
+        else
+            return;
+    }//end guess method
+}//end class
